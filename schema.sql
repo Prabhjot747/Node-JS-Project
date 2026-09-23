95c90e59b5cfb264590f80aa2e658bed86bd3c06
Prabhjot Singh <prabhjot.singh@mobileprogramming.com>
2026-09-23 19:18:07 +0530

    implemented foreign key

--- schema.sql
+++ schema.sql
@@ -1,9 +1,39 @@
 CREATE DATABASE royalenfieldeichermotordivison;
 USE royalenfieldeichermotordivison;
 
+-- Motorcycle Table
 CREATE TABLE motorcycle(
 id INT  PRIMARY KEY,
 motorcycleName VARCHAR(20) NOT NULL UNIQUE,
 createdAt DATETIME  NOT NULL COMMENT "created time",
 updatedAt DATETIME  NOT NULL COMMENT "updated time"
 )
+
+-- Spare part
+CREATE TABLE engines(
+id INT  PRIMARY KEY,
+name VARCHAR(20) NOT NULL UNIQUE,
+dimensionLength NUMERIC ,
+dimensionWidth NUMERIC ,
+dimensionHeight NUMERIC ,
+strokeWidth NUMERIC ,
+boreWidth NUMERIC ,
+maximumPowerBHP NUMERIC ,
+maximumPowerHP NUMERIC ,
+ignitionSystem VARCHAR(20) NOT NULL,
+gearBox VARCHAR(20) NOT NULL,
+engineOil VARCHAR(20) NOT NULL,
+engineStart VARCHAR(10) NOT NULL,
+engineDisplacement NUMERIC ,
+maximumTorqueNM NUMERIC ,
+maximumTorqueRPM  NUMERIC ,
+clutch VARCHAR(10) NOT NULL,
+lubrication VARCHAR(20) NOT NULL,
+airCleaner VARCHAR(10) NOT NULL,
+createdAt DATETIME  NOT NULL COMMENT "created time",
+updatedAt DATETIME  NOT NULL COMMENT "updated time",
+motorcycleId INT, FOREIGN KEY (motorcycleId) REFERENCES motorcycle(id)  
+);
+
+
+
